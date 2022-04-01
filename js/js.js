e9935799c881c46991b1e349f77538c94b247089
Kasemer <86676174+Kasemer@users.noreply.github.com>
2022-04-01 10:24:08 +0300

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,109 @@
+$( document ).ready(function(){
+	  $("button").click(function(){ // задаем функцию при нажатиии на элемент <button>
+	   $('.how').toggleClass('yes' ); // удаляем, или добавляем элементу <p> два класса
+	  });
+	});
+
+
+
+
+
+// АНИМАЦИЯ ОДИН control
+	$( document ).ready(function(){
+		  $('.circle').click(function(){ // задаем функцию при нажатиии на элемент <button>
+		   $('.circle').toggleClass('big'); // удаляем, или добавляем элементу <p> два класса
+		  });
+		});
+
+
+		$(document).ready(function(){
+		  $('.circle').click(function(){
+		    $('.fff').fadeToggle();
+
+		  });
+	});
+
+
+
+
+
+		// АНИМАЦИЯ ДВА control
+			$( document ).ready(function(){
+				  $('.circle2').click(function(){ // задаем функцию при нажатиии на элемент <button>
+				   $('.circle2').toggleClass('big'); // удаляем, или добавляем элементу <p> два класса
+				  });
+				});
+				$(document).ready(function(){
+					$('.circle2').click(function(){
+						$('.cc').fadeToggle();
+
+					});
+				});
+
+				// АНИМАЦИЯ ТРИ control
+					$( document ).ready(function(){
+							$('.circle3').click(function(){ // задаем функцию при нажатиии на элемент <button>
+							 $('.circle3').toggleClass('big'); // удаляем, или добавляем элементу <p> два класса
+							});
+						});
+
+
+						$(document).ready(function(){
+							$('.circle3').click(function(){
+								$('.An3').fadeToggle();
+
+							});
+						});
+
+				//
+						// АНИМАЦИЯ ЧЕТЫРЕ control
+							$( document ).ready(function(){
+									$('.circle4').click(function(){ // задаем функцию при нажатиии на элемент <button>
+									 $('.circle4').toggleClass('big'); // удаляем, или добавляем элементу <p> два класса
+									});
+								});
+
+
+								$(document).ready(function(){
+									$('.circle4').click(function(){
+										$('.an4').fadeToggle();
+
+									});
+								});
+
+
+
+
+
+
+								// АНИМАЦИЯ ПЯТАЯ control
+									$( document ).ready(function(){
+											$('.circle5').click(function(){ // задаем функцию при нажатиии на элемент <button>
+											 $('.circle5').toggleClass('big'); // удаляем, или добавляем элементу <p> два класса
+											});
+										});
+
+
+										$(document).ready(function(){
+											$('.circle5').click(function(){
+												$('.an5').fadeToggle();
+
+											});
+										});
+
+
+
+										// АНИМАЦИЯ ПЯТАЯ control
+											$( document ).ready(function(){
+													$('.circle6').click(function(){ // задаем функцию при нажатиии на элемент <button>
+													 $('.circle6').toggleClass('big'); // удаляем, или добавляем элементу <p> два класса
+													});
+												});
+
+
+												$(document).ready(function(){
+													$('.circle6').click(function(){
+														$('.an6').fadeToggle();
+
+													});
+												});
